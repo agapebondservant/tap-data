27e2aa4f2eb3ab997fc36938b32851657e64cd94
Tola Awofolu <oawofolu@pivotal.io>
2022-06-06 17:58:42 -0400

    Updated configmap

--- workshop/setup.d/env-subst.sh
+++ workshop/setup.d/env-subst.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl get configmap data-e2e-env -ojson | jq ".data" | sed 's/[{}]//g' > .env-properties
+kubectl get configmap data-e2e-env -ndefault -ojson | jq ".data" | sed 's/[{}]//g' > .env-properties
 source .env-properties
 for orig in `find ~ -name "*.in.*" -type f`; do
   target=$(echo $orig | sed 's/\.in//')
