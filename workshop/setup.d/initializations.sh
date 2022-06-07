d22edcdc9e715ee21e51020a6621d20e126052b9
Tola Awofolu <oawofolu@pivotal.io>
2022-06-06 20:18:03 -0400

    Updated script

--- workshop/setup.d/initializations.sh
+++ workshop/setup.d/initializations.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -eo pipefail
 
 kubectl get configmap data-e2e-env -ndefault -ojson | jq -r ".data | to_entries[] | [.key, .value] | join(\"=\")" | sed 's/^/export /' > ~/.env-properties
+cat ~/.env-properties
 source ~/.env-properties
 for orig in `find ~ -name "*.in.*" -type f`; do
   target=$(echo $orig | sed 's/\.in//')
