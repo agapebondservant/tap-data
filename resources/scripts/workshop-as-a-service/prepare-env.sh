cdd774cdb299cb6d2de25da6643207bdb0a1785d
Tola Awofolu <oawofolu@pivotal.io>
2022-08-26 15:36:38 -0400

    Fixed syntax error

--- resources/scripts/workshop-as-a-service/prepare-env.sh
+++ resources/scripts/workshop-as-a-service/prepare-env.sh
@@ -7,7 +7,7 @@ source "$ENV_FILE"
 for orig in `find $(pwd) -name "*.in.*" -type f`; do
   target=$(echo $orig | sed 's/\.in//')
   envsubst < $orig > $target
-  grep $-qxF target .gitignore || echo $target >> .gitignore
+  grep -qxF $target .gitignore || echo $target >> .gitignore
   git rm --cached -q $target > /dev/null 2>&1
 done
 echo "Environment variables updated."
