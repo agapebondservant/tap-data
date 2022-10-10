6bcdfa2f71876d0905ba4867dc72c5ca5ff24a9d
Tola Awofolu <oawofolu@pivotal.io>
2022-10-10 17:07:59 -0400

    DDL schema changes

---
+++ other/resources/gemfire/java-source/src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+BEGIN
+   EXECUTE IMMEDIATE 'DROP TABLE claims';
+EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN
+         RAISE;
+      END IF;
+END;
+
+
+CREATE TABLE claims (
+    id number(9,0),
+    name varchar2(100),
+    dob varchar2(10),
+    address varchar2(250),
+    phone varchar2(50),
+    claimdate varchar2(10),
+    city varchar2(50),
+    amount number(9,0)
+);
