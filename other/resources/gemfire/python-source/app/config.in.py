d4590e63b35e9d9eb178a6251e3cc199d66945bd
Tola Awofolu <oawofolu@pivotal.io>
2022-10-27 10:56:01 -0400

    Gemfire content updates

--- other/resources/gemfire/python-source/app/config.in.py
+++ other/resources/gemfire/python-source/app/config.in.py
@@ -5,3 +5,4 @@ primary = {
 secondary = {
     'oracle': 'oracle+cx_oracle://$DATA_E2E_ORACLE_DB_SECONDARY_USER:$DATA_E2E_ORACLE_DB_SECONDARY_PASS@$DATA_E2E_ORACLE_DB_SECONDARY_HOST:1521/?service_name=orcl'
 }
+
