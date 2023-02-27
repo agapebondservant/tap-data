eee47bebdb4d9c65403bf5079558ae58235fe044
Tola Awofolu <oawofolu@pivotal.io>
2023-02-27 14:25:10 -0500

    Updates related to Greenplum operator

--- resources/scripts/setup.sh
+++ resources/scripts/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Delete pre-existing Greenplum operator and PXF-service
 kubectl delete greenplumpxfservice data-e2e-greenplum-pxf --namespace greenplum-system --ignore-not-found=true
-kubectl delete greenplumcluster gpdb-cluster-data-samples-w01-s001 --ignore-not-found=true --namespace greenplum-system
+kubectl delete greenplumcluster --all --ignore-not-found=true --namespace greenplum-system
 helm uninstall greenplum-operator --namespace greenplum-system
 
 # Install  Greenplum operator
 kubectl create ns greenplum-system --dry-run -o yaml | kubectl apply -f - && \
+kubectl label namespaces greenplum-system cert-manager.io/disable-validation=true
 kubectl create secret docker-registry image-pull-secret --namespace=greenplum-system \
 --docker-username='${DATA_E2E_REGISTRY_USERNAME}' --docker-password='${DATA_E2E_REGISTRY_PASSWORD}' \
 --dry-run -o yaml | kubectl apply -f -
--- workshop/content/mlops-tap-overview.md
+++ workshop/content/mlops-tap-overview.md
@@ -14,7 +14,7 @@ In this session, we will demonstrate how **Tanzu Application Platform** enables 
 We will build an image classifier that will be able to identify objects by name.
 Our classifier will be trained on a limited number of *labels* from the well-known **CIFAR 10** dataset.
 For the purposes of this exercise, we will build our model from scratch using the popular **Tensorflow (Keras)** framework.
-We will demonstrate using a pre-trained model in a future exercise.
+We will demonstrate **transfer learning** (using a *pre-trained model*) in a future exercise.
 
 #### Training a Convolutional Neural Network on TAP
 ![Training a Convolutional Neural Network on TAP](images/mlops-usecase-overview.jpg)
