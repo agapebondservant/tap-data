1256afa973d90b5f504409fbd8ff05afa5a1daa3
Tola Awofolu <oawofolu@pivotal.io>
2023-12-06 11:29:24 -0500

    Enabled docker

--- workshop/setup.d/initializations.sh
+++ workshop/setup.d/initializations.sh
@@ -85,8 +85,8 @@ if [ $? = 0 ]; then echo "Pre-existing accelerators were deleted."; else echo "A
 
 echo "Setting up databases..."
 # Database
-ctr run --rm postgres psql ${DATA_E2E_ML_INFERENCE_DB_CONNECT} -c "DROP SCHEMA IF EXISTS \"${SESSION_NAMESPACE}\"; CREATE SCHEMA \"${SESSION_NAMESPACE}\";" || true
-ctr run --rm postgres psql ${DATA_E2E_ML_TRAINING_DB_CONNECT} -c "DROP SCHEMA IF EXISTS \"${SESSION_NAMESPACE}\"; CREATE SCHEMA \"${SESSION_NAMESPACE}\";" || true
+docker run --rm postgres psql ${DATA_E2E_ML_INFERENCE_DB_CONNECT} -c "DROP SCHEMA IF EXISTS \"${SESSION_NAMESPACE}\"; CREATE SCHEMA \"${SESSION_NAMESPACE}\";" || true
+docker run --rm postgres psql ${DATA_E2E_ML_TRAINING_DB_CONNECT} -c "DROP SCHEMA IF EXISTS \"${SESSION_NAMESPACE}\"; CREATE SCHEMA \"${SESSION_NAMESPACE}\";" || true
 echo "Databases set up."
 
 
