6067efc08710dc1a365d6b1deb8b40382c740515
Tola Awofolu <oawofolu@pivotal.io>
2024-01-02 06:41:45 -0500

    Updated PlPython training script

--- other/resources/plpython/sql/deploy_db_training.sql
+++ other/resources/plpython/sql/deploy_db_training.sql
@@ -29,14 +29,6 @@ AS $$
         os.environ['experiment_name']=experiment_name
         os.environ['shared_app_path']=app_location
         os.environ['PYTHONPATH']=f'{app_location}/_vendor:{app_location}:{os.getenv("PYTHONPATH")}:{os.getenv("PATH")}'
---        sys.path.append(f'{app_location}/_vendor')
---        sys.path.append(f'{app_location}')
---        if sys.modules.get('base_app.base_app_main'):
---            del sys.modules['base_app.base_app_main']
---        if sys.modules.get('base_app'):
---            del sys.modules['base_app']
-
---        from base_app import base_app_main
 
         result = subprocess.run([sys.executable, f"{app_location}/base_app/main.py"], capture_output=True, text=True, env=os.environ.copy())
         plpy.info("stdout: ", result.stdout)
